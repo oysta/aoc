f0085e2d84211dc2ddfe22851687d425b6380714
Christopher Owen <mail@christopherowen.id.au>
2022-12-06 10:27:22 +1100

    Use a better array literal syntax

--- 2022/day2/strategise.rb
+++ 2022/day2/strategise.rb
@@ -1,14 +1,14 @@
 SCORES = { "X" => 1, "Y" => 2, "Z" => 3 }
 
-OPP = ["A", "B", "C"]
-MINE = ["X", "Y", "Z"]
+OPP = %w[A B C]
+MINE = %w[X Y Z]
 
 DRAWS = OPP.zip(MINE)
 WINS = OPP.zip(MINE.rotate)
 
 lines = ARGF.readlines.map(&:chomp).map(&:split)
 
-selection_points = lines.map { |pair| pair[1] }.map(&SCORES).sum
+selection_points = lines.map { _1[1] }.map(&SCORES).sum
 draw_points = lines.count(&DRAWS.method(:include?)) * 3
 win_points = lines.count(&WINS.method(:include?)) * 6
 
