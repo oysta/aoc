98272c8fcc2b602860745c22895a26efcbdc0dc7
Christopher Owen <mail@christopherowen.id.au>
2022-12-05 17:10:47 +1100

    Make more compact via procs

--- 2022/day2/strategise.rb
+++ 2022/day2/strategise.rb
@@ -1,15 +1,11 @@
-lines = ARGF.readlines.map(&:chomp)
-
 SCORES = { "X" => 1, "Y" => 2, "Z" => 3 }
-RESULTS = { win: 6, draw: 3, loss: 0 }
 WINS = [["A", "Y"], ["B", "Z"], ["C", "X"]]
 DRAWS = [["A", "X"], ["B", "Y"], ["C", "Z"]]
 
-lines = lines.map { |l| l.split(" ") }
+lines = ARGF.readlines.map(&:chomp).map { _1.split(" ") }
 
-selection_points = lines.map { |pair| pair[1] }.map { SCORES[_1] }.sum
-
-draw_points = lines.count { |pair| DRAWS.include?(pair) } * 3
-win_points = lines.count { |pair| WINS.include?(pair) } * 6
+selection_points = lines.map { |pair| pair[1] }.map(&SCORES).sum
+draw_points = lines.count(&DRAWS.method(:include?)) * 3
+win_points = lines.count(&WINS.method(:include?)) * 6
 
 puts selection_points + win_points + draw_points
