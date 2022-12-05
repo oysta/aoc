1dd5213eb812997294fcfce2be00af779187f548
Christopher Owen <mail@christopherowen.id.au>
2022-12-05 17:00:44 +1100

    Use count function directly

--- 2022/day2/strategise.rb
+++ 2022/day2/strategise.rb
@@ -5,13 +5,11 @@ RESULTS = { win: 6, draw: 3, loss: 0 }
 WINS = [["A", "Y"], ["B", "Z"], ["C", "X"]]
 DRAWS = [["A", "X"], ["B", "Y"], ["C", "Z"]]
 
-
 lines = lines.map { |l| l.split(" ") }
-
 
 selection_points = lines.map { |pair| pair[1] }.map { SCORES[_1] }.sum
 
-draw_points = lines.select { |pair| DRAWS.include?(pair) }.count * 3
-win_points = lines.select { |pair| WINS.include?(pair) }.count * 6
+draw_points = lines.count { |pair| DRAWS.include?(pair) } * 3
+win_points = lines.count { |pair| WINS.include?(pair) } * 6
 
 puts selection_points + win_points + draw_points
