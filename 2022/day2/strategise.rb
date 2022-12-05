ee284bad56f4d1d688db52a42cc837252dd7414a
Christopher Owen <mail@christopherowen.id.au>
2022-12-05 17:14:01 +1100

    Use default split (I seem to always forget default behaviour)

--- 2022/day2/strategise.rb
+++ 2022/day2/strategise.rb
@@ -2,7 +2,7 @@ SCORES = { "X" => 1, "Y" => 2, "Z" => 3 }
 WINS = [["A", "Y"], ["B", "Z"], ["C", "X"]]
 DRAWS = [["A", "X"], ["B", "Y"], ["C", "Z"]]
 
-lines = ARGF.readlines.map(&:chomp).map { _1.split(" ") }
+lines = ARGF.readlines.map(&:chomp).map(&:split)
 
 selection_points = lines.map { |pair| pair[1] }.map(&SCORES).sum
 draw_points = lines.count(&DRAWS.method(:include?)) * 3
