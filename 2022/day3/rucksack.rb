499167d698b93a41111b329f22c2e4efb5809923
Christopher Owen <mail@christopherowen.id.au>
2022-12-07 16:41:33 +1100

    Use implicit parameter names

--- 2022/day3/rucksack.rb
+++ 2022/day3/rucksack.rb
@@ -1,12 +1,12 @@
 lines = ARGF.readlines.map(&:chomp)
 
 PRIORITIES = Hash[
-  (('a'..'z').to_a + ('A'..'Z').to_a).each_with_index.map { |l,i| [l, i.succ] }
+  (('a'..'z').to_a + ('A'..'Z').to_a).each_with_index.map { [_1, _2.succ] }
 ]
 
 puts lines
   .map { |line| line.chars.each_slice(line.size / 2).to_a }
-  .map {|c1, c2| c1.to_a.intersection(c2.to_a) }
+  .map { _1.to_a & _2.to_a }
   .map(&:first)
   .map(&PRIORITIES) 
   .sum
