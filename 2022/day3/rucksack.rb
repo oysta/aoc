4a8b3b22712a354d4601487e0bf917e72e20e98c
Christopher Owen <mail@christopherowen.id.au>
2022-12-07 20:11:36 +1100

    Use more implicit paramater names

--- 2022/day3/rucksack.rb
+++ 2022/day3/rucksack.rb
@@ -5,7 +5,7 @@ PRIORITIES = Hash[
 ]
 
 puts lines
-  .map { |line| line.chars.each_slice(line.size / 2).to_a }
+  .map { _1.chars.each_slice(_1.size / 2).to_a }
   .map { _1.to_a & _2.to_a }
   .map(&:first)
   .map(&PRIORITIES) 
