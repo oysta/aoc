7fa397ee0ecf48d47506eb100cb7574ae6156100
Christopher Owen <mail@christopherowen.id.au>
2022-12-13 09:57:58 +1100

    Day 6 Part 2

--- 2022/day6/signal.rb
+++ 2022/day6/signal.rb
@@ -1,12 +1,12 @@
 buf = []; idx = 0
 
+detection_size = 14
+
 while ch = STDIN.getc
     buf << ch
     idx = idx.succ
-    if buf.size > 4
-      buf.shift
-    end
-    if buf.uniq.size == 4
+    buf.shift if buf.size > detection_size
+    if buf.uniq.size == detection_size
       puts idx
       exit 0
     end
