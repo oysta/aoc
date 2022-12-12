ad23cbf9fcc161e1543af00b1ef6a96bda99eeab
Christopher Owen <mail@christopherowen.id.au>
2022-12-12 20:38:16 +1100

    Simplify Day 5 Part 1

--- 2022/day5/stacking.rb
+++ 2022/day5/stacking.rb
@@ -1,7 +1,4 @@
-
-scheme, instructions = ARGF.readlines("").take(2)
-
-crates = scheme
+crates = ARGF.readline("")
     .split($/)
     .map { _1.ljust(9 * 4, " ") } # Not technically needed on my input
     .map(&:chars)
@@ -11,14 +8,9 @@ stacks = 9.times.map do |i|
   crates[1 + i * 4].reverse[1..-1].reject { _1 == " " }
 end
 
-instructions.split($/).each do |instruction|
+ARGF.readlines.each do |instruction|
   quantity, from, to = instruction.scan(/\d+/).map(&:to_i)
   stacks[to - 1].concat(stacks[from - 1].pop(quantity).reverse)
 end
 
 puts stacks.flat_map(&:last).join("")
-
-
-
-
-
