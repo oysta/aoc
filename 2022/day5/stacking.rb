2deb1d931fc2536824719253f4bb2f108477629e
Christopher Owen <mail@christopherowen.id.au>
2022-12-12 20:34:33 +1100

    Day 5 Part 1

---
+++ 2022/day5/stacking.rb
@@ -0,0 +1,24 @@
+
+scheme, instructions = ARGF.readlines("").take(2)
+
+crates = scheme
+    .split($/)
+    .map { _1.ljust(9 * 4, " ") } # Not technically needed on my input
+    .map(&:chars)
+    .transpose
+
+stacks = 9.times.map do |i| 
+  crates[1 + i * 4].reverse[1..-1].reject { _1 == " " }
+end
+
+instructions.split($/).each do |instruction|
+  quantity, from, to = instruction.scan(/\d+/).map(&:to_i)
+  stacks[to - 1].concat(stacks[from - 1].pop(quantity).reverse)
+end
+
+puts stacks.flat_map(&:last).join("")
+
+
+
+
+
