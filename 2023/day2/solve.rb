b5e6bad79c36edca2575288bb76536417e945b20
Christopher Owen <mail@christopherowen.id.au>
2023-12-03 17:40:40 +1100

    2023 day 2 part 2

--- 2023/day2/solve.rb
+++ 2023/day2/solve.rb
@@ -2,7 +2,7 @@ input = ARGF.readlines
 
 max = { "red": 12, "green": 13, "blue": 14}
 
-puts input.map { |line| 
+puts input.dup.map { |line| 
   line[/^Game /] = ""
   id,_,draws = line.partition(":")
   if draws.split(";").none? { |draw| draw.split(",").any? { |group| num, col = group.split; num.to_i > max[col.to_sym] } }
@@ -12,4 +12,17 @@ puts input.map { |line|
   end
 }
 .compact
+.sum
+
+puts input.dup.map { |line| 
+  _,_,draws = line.partition(":")
+  draws.split(";").flat_map { |draw| 
+    draw.split(",").map(&:split) 
+  }
+  .group_by(&:last)
+  .map { |_,v| v.max_by { |count| count[0].to_i }  }
+  .map(&:first)
+  .map(&:to_i)
+  .reduce(:*)
+}
 .sum
