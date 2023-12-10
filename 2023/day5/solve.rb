0601ae98614ce0417d588a169ceada89966fe484
Christopher Owen <mail@christopherowen.id.au>
2023-12-10 11:59:24 +1100

    2023 day 5 part 1 clean up

--- 2023/day5/solve.rb
+++ 2023/day5/solve.rb
@@ -5,16 +5,16 @@ seeds = input.shift.split(": ").last.split.map(&:to_i)
 maps = input
   .chunk_while { !_2.empty? }
   .map { |chunk| 
-    chunk[2..].map { _1.split.map(&:to_i) } 
+    chunk[2..]
+      .map { _1.split.map(&:to_i) }
+      .each { _1[1] = (_1[1]..._1[1]+_1[2]) } 
   }
 
 results = seeds.map { |seed| 
-  value = seed
-  maps.each { |map|  
-    mapping = map.find { (_1[1]..._1[1]+_1[2]).member? value }
-    value = mapping[0] + (value - mapping[1]) unless mapping.nil?
+  maps.reduce(seed) { |value, map| 
+    mapping = map.find { |_, source, _| source === value }
+    mapping ? mapping[0] + (value - mapping[1].begin) : value
   }
-  value
 }
 
 puts results.min
