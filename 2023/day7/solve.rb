5966a17478f0aa80b003a21d4e0089de87dbdeca
Christopher Owen <mail@christopherowen.id.au>
2023-12-12 22:50:18 +1100

    2023 day 7 part 1

---
+++ 2023/day7/solve.rb
@@ -0,0 +1,22 @@
+input = ARGF.readlines.map(&:chomp)
+
+CARD_STRENGTH = %w[2 3 4 5 6 7 8 9 T J Q K A]
+
+ranked = input
+  .map(&:split)
+  .map { |hand,bid| [hand.chars, bid.to_i] }
+  .map { |chars, bid| [chars, chars.tally.values.sort, bid] }
+  .sort { |a, b| 
+    counts1, counts2 = b[1], a[1]
+
+    if counts1.count != counts2.count
+      counts2 <=> counts1
+    elsif counts1 != counts2
+      counts2.last <=> counts1.last
+    else
+      diff = b[0].zip(a[0]).find { _1[0] != _1[1] }
+      CARD_STRENGTH.index(diff[1]) <=> CARD_STRENGTH.index(diff[0])
+    end
+  }
+
+puts ranked.each_with_index.map {|hand, idx| (idx+1) * hand[2] }.sum
