4309577ba9b8fa7b7c4746f23e8dffd199b20957
Christopher Owen <mail@christopherowen.id.au>
2023-12-13 13:19:54 +1100

    Use better destructuring, complete pipeline

--- 2023/day7/solve.rb
+++ 2023/day7/solve.rb
@@ -7,15 +7,16 @@ STRENGTH_COMP = lambda { |a, b|
   CARD_STRENGTH[diff[1]] <=> CARD_STRENGTH[diff[0]]
 }
 
-ranked = input
+puts input
   .map(&:split)
   .map { |hand, bid| [hand.chars, bid.to_i] }
   .map { |chars, bid| [chars, chars.tally.values, bid] }
   .map { |chars, tallies, bid| [chars, [5-tallies.count,tallies.max], bid] }
-  .sort { |a, b| a[1] <=> b[1] }
-  .group_by { _1[1] }
-  .transform_values  { |same_kinded| same_kinded.sort(&STRENGTH_COMP) }
+  .sort_by { |_, kind| kind }
+  .group_by { |_, kind| kind }
+  .transform_values { |same_kinded| same_kinded.sort(&STRENGTH_COMP) }
   .values
   .flatten(1)
-
-puts ranked.map.with_index { |hand, idx| (idx+1) * hand[2] }.sum
+  .map
+  .with_index { |(_,_,bid), idx| (idx+1) * bid }
+  .sum
