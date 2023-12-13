817178946da5ec8caebb447ea06d7b64469e2a0c
Christopher Owen <mail@christopherowen.id.au>
2023-12-13 23:15:51 +1100

    2023 day 8 part 1

---
+++ 2023/day8/solve.rb
@@ -0,0 +1,27 @@
+input = ARGF.readlines.map(&:chomp)
+
+directions = input.shift
+
+input.shift
+
+ROUTE_RE = /([A-Z]{3}) = \(([A-Z]{3}), ([A-Z]{3})\)/
+
+ROUTES = Hash[
+  input
+    .map(&ROUTE_RE.method(:match))
+    .map(&:captures)
+    .map { |key, left, right| [key, {"L" => left, "R" => right}] }
+    .to_a
+]
+
+now_at = 'AAA'
+
+puts directions
+  .chars
+  .lazy
+  .cycle
+  .take_while { |direction| 
+    now_at = ROUTES[now_at][direction]
+    now_at != 'ZZZ' 
+  }
+  .count + 1
