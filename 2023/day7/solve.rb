6efbe2aadff103ecf57c2c500d4eb1da5f8e0d14
Christopher Owen <mail@christopherowen.id.au>
2023-12-13 13:07:21 +1100

    More functionaly pipe line of sorting hands based on type and then rank

--- 2023/day7/solve.rb
+++ 2023/day7/solve.rb
@@ -2,19 +2,20 @@ input = ARGF.readlines.map(&:chomp)
 
 CARD_STRENGTH = Hash[%w[2 3 4 5 6 7 8 9 T J Q K A].each.with_index.to_a]
 
+STRENGTH_COMP = lambda { |a, b| 
+  diff = b[0].zip(a[0]).find { _1 != _2 }
+  CARD_STRENGTH[diff[1]] <=> CARD_STRENGTH[diff[0]]
+}
+
 ranked = input
   .map(&:split)
   .map { |hand, bid| [hand.chars, bid.to_i] }
   .map { |chars, bid| [chars, chars.tally.values, bid] }
   .map { |chars, tallies, bid| [chars, [5-tallies.count,tallies.max], bid] }
-  .sort { |a, b| 
-    comp = (a[1] <=> b[1])
-    if comp == 0
-      diff = b[0].zip(a[0]).find { _1 != _2 }
-      CARD_STRENGTH[diff[1]] <=> CARD_STRENGTH[diff[0]]
-    else
-      comp
-    end
-  }
+  .sort { |a, b| a[1] <=> b[1] }
+  .group_by { _1[1] }
+  .transform_values  { |same_kinded| same_kinded.sort(&STRENGTH_COMP) }
+  .values
+  .flatten(1)
 
 puts ranked.map.with_index { |hand, idx| (idx+1) * hand[2] }.sum
