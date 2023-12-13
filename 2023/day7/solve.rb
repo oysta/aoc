58295b6f0c1626a65ce2f51e419d6df55232b448
Christopher Owen <mail@christopherowen.id.au>
2023-12-13 12:04:08 +1100

    Simplify sort

--- 2023/day7/solve.rb
+++ 2023/day7/solve.rb
@@ -4,19 +4,17 @@ CARD_STRENGTH = Hash[%w[2 3 4 5 6 7 8 9 T J Q K A].each.with_index.to_a]
 
 ranked = input
   .map(&:split)
-  .map { |hand,bid| [hand.chars, bid.to_i] }
-  .map { |chars, bid| [chars, chars.tally.values.sort, bid] }
+  .map { |hand, bid| [hand.chars, bid.to_i] }
+  .map { |chars, bid| [chars, chars.tally.values, bid] }
+  .map { |chars, tallies, bid| [chars, [5-tallies.count,tallies.max], bid] }
   .sort { |a, b| 
-    counts1, counts2 = b[1], a[1]
-
-    if counts1.count != counts2.count
-      counts2 <=> counts1
-    elsif counts1 != counts2
-      counts2.last <=> counts1.last
-    else
+    comp = (a[1] <=> b[1])
+    if comp == 0
       diff = b[0].zip(a[0]).find { _1 != _2 }
       CARD_STRENGTH[diff[1]] <=> CARD_STRENGTH[diff[0]]
+    else
+      comp
     end
   }
 
-puts ranked.map.with_index {|hand, idx| (idx+1) * hand[2] }.sum
+puts ranked.map.with_index { |hand, idx| (idx+1) * hand[2] }.sum
