efb537626584100b2d43e61cf19f528335b291fd
Christopher Owen <mail@christopherowen.id.au>
2023-12-16 14:44:07 +1100

    2023 day 9 part 1

---
+++ 2023/day9/solve.rb
@@ -0,0 +1,27 @@
+input = ARGF.readlines.map(&:chomp)
+
+def diffs(enum)
+  Enumerator.new do |yielder|
+    loop do
+      yielder.yield enum
+      enum = enum.each_cons(2).map{ |a,b| b-a }
+      break unless enum.any?(&:nonzero?)
+    end
+    yielder.yield enum
+  end
+end
+
+p input
+  .map { |line| line.split.map(&:to_i) }
+  .map { |seq| 
+    diffs(seq)
+      .reverse_each
+      .to_a
+      .each_cons(2) { |a,b| b << b.last + a.last }
+      .to_a
+      .last
+      .last
+  }
+}
+.sum
+
